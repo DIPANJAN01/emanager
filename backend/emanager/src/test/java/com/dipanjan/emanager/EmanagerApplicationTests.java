69334b06a741a8ade3d318b50d2ae3a0c1420615
Dipanjan <dipanjanghosal01@gmail.com>
2024-02-08 13:28:27 +0530

    initial commit

---
+++ backend/emanager/src/test/java/com/dipanjan/emanager/EmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipanjan.emanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
